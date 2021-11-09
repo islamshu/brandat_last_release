ac6579a89a3be752cf7c6988ee4bc31b4a62a2b7
islamshu12 <islamshu12@gmail.com>
2021-11-09 14:27:55 +0200

    سس

--- app/Models/V3/Category.php
+++ app/Models/V3/Category.php
@@ -41,7 +41,15 @@ class Category extends Model
             $builder->orderBy('name', 'asc');
         });
     }
+    public function getTranslation($field = '', $lang = false){
+        $lang = $lang == false ? App::getLocale() : $lang;
+        $category_translation = $this->hasMany(CategoryTranslation::class)->where('lang', $lang)->first();
+        return $category_translation != null ? $category_translation->$field : $this->$field;
+    }
 
+    public function category_translations(){
+    	return $this->hasMany(CategoryTranslation::class);
+    }
     public function subCategories()
     {
         return $this->hasMany(SubCategory::class);
